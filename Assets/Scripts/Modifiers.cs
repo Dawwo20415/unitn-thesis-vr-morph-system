0f0523dd5e5ea32213bb55c5fc323262e49781b3
Dawwo-15 <davidemongera@hotmail.com>
2023-08-17 18:45:52 +0200

    Update from laptop to desktop

---
+++ Assets/Scripts/Modifiers.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="TestName", menuName="ScriptableObjects/Item1")]
+public class Modifiers : ScriptableObject
+{
+
+}
