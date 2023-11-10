0aa869e2e4cb5235db4e65c2be0e03ba313a124e
Dawwo-15 <davidemongera@hotmail.com>
2023-11-10 21:07:20 +0100

    Ultimated Follow algorithm

--- Assets/Test2.cs
+++ Assets/Test2.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Mathematics;
 
-public class FollowTest : MonoBehaviour
+public class Test2 : MonoBehaviour
 {
     public List<Transform> points;
 
@@ -54,34 +53,45 @@ public class FollowTest : MonoBehaviour
         Vector3 N1 = Vector3.Cross(AB, AC).normalized;
         Vector3 N2 = Vector3.Cross(nAB, nAC).normalized;
 
-        Debug.DrawLine(nA, nA + AB, Color.white, Time.deltaTime, false);
-        Debug.DrawLine(nA, nA + AC, Color.white, Time.deltaTime, false);
+        float d1 = -(N1.x * A.x) - (N1.y * A.y) - (N1.z * A.z);
+        float d2 = -(N2.x * nA.x) - (N2.y * nA.y) - (N2.z * nA.z);
+
+        Quaternion referencePlane = new Quaternion(N1.x, N1.y, N1.z, 0.0f);
+        Quaternion newPlane = new Quaternion(N2.x, N2.y, N2.z, 1.0f);
+
+        //Debug.DrawLine(nA, nA + AB, Color.white, Time.deltaTime, false);
+        //Debug.DrawLine(nA, nA + AC, Color.white, Time.deltaTime, false);
         Debug.DrawLine(nA, nA + N1, Color.white, Time.deltaTime, false);
 
         Debug.DrawLine(nA, nA + nAB, Color.green, Time.deltaTime, false);
         Debug.DrawLine(nA, nA + nAC, Color.green, Time.deltaTime, false);
         Debug.DrawLine(nA, nA + N2, Color.green, Time.deltaTime, false);
 
-        Vector3 M = Vector3.Cross(N1, N2);
 
-        
-        Debug.DrawLine(Vector3.zero, AB, Color.red, Time.deltaTime, false);
-        Debug.DrawLine(Vector3.zero, nAB, Color.red, Time.deltaTime, false);
-        Debug.DrawLine(Vector3.zero, N1, Color.red, Time.deltaTime, false);
+        //Debug.DrawLine(Vector3.zero, AB, Color.red, Time.deltaTime, false);
+        //Debug.DrawLine(Vector3.zero, nAB, Color.red, Time.deltaTime, false);
+        //Debug.DrawLine(Vector3.zero, N1, Color.red, Time.deltaTime, false);
 
-        Quaternion rotation = Quaternion.FromToRotation(N1,N2);
+        Quaternion rotation1 = Quaternion.FromToRotation(N1, N2);
+        float angle = Vector3.SignedAngle(rotation1 * AB, nAB, N2);
+        Debug.Log("Angle: " + angle.ToString(), this);
+        Debug.DrawLine(nA, nA + (rotation1 * AB), Color.cyan, Time.deltaTime, false);
+        Debug.DrawLine(nA, nA + (rotation1 * AC), Color.cyan, Time.deltaTime, false);
+        Quaternion rotation2 = Quaternion.FromToRotation(rotation1 * AB, nAB);
+        Debug.DrawLine(nA, nA + (rotation2 * rotation1 * AB), Color.black, Time.deltaTime, false);
+        Debug.DrawLine(nA, nA + (rotation2 * rotation1 * AC), Color.black, Time.deltaTime, false);
 
-        float angle = Vector3.SignedAngle(rotation * AB, rotation * nAB, N1);
+        Quaternion rotation = rotation2 * rotation1;
 
-        rotation *= Quaternion.AngleAxis(-angle, N1);
 
-        Debug.DrawLine(nA, nA + (rotation * AB), Color.magenta, Time.deltaTime, false);
-        Debug.DrawLine(nA, nA + (rotation * AC), Color.magenta, Time.deltaTime, false);
+
+        //Debug.DrawLine(nA, nA + (rotation * AB), Color.magenta, Time.deltaTime, false);
+        //Debug.DrawLine(nA, nA + (rotation * AC), Color.magenta, Time.deltaTime, false);
         Debug.DrawLine(nA, nA + (rotation * N1), Color.magenta, Time.deltaTime, false);
 
         for (int i = 0; i < points.Count; i++)
         {
-            //Debug.DrawLine(midpoint, midpoint + (rotation * references[i]), Color.magenta, Time.deltaTime, false);
+            Debug.DrawLine(midpoint, midpoint + (rotation * references[i]), Color.magenta, Time.deltaTime, false);
             //Debug.DrawLine(midpoint, points[i].position, Color.red, Time.deltaTime, false);
             Debug.DrawLine(midpoint, midpoint + references[i], Color.blue, Time.deltaTime, false);
         }
