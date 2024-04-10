b3760629a2ec3a9f3947fd5437555f624b72e155
Dawwo-15 <davidemongera@hotmail.com>
2024-04-10 22:26:57 +0200

    Extended implementation of PoseBehaviour

--- Assets/Scripts/PlayablesAPI Stuff/Tests/RawRotationTesting.cs
+++ Assets/Scripts/PlayablesAPI Stuff/Tests/RawRotationTesting.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 using UnityEngine.XR.OpenXR.Input;
 
 /// <summary>
-/// TODO create the graph, create the single quaternion modifiers and create the merger
+/// TODO create the graph, cre ate the single quaternion modifiers and create the merger
 /// </summary>
 
 public class PoseBehaviour : PlayableBehaviour
@@ -83,29 +83,45 @@ public class TPoseBehaviour : PoseBehaviour
 public struct PoseToAnimation : IAnimationJob
 {
 
-    private PoseBehaviour pose;
+    private ScriptPlayable<PoseBehaviour> pose_playable;
     private NativeArray<TransformStreamHandle> bones;
     private int count;
 
-    public void Setup(Animator animator, List<Transform> bones_trn, PoseBehaviour input)
+    public void Setup(Animator animator, List<Transform> bones_trn)
     {
         bones = new NativeArray<TransformStreamHandle>(bones_trn.Count, Allocator.Persistent);
         for (int i = 0; i < bones_trn.Count; i++) { bones[i] = animator.BindStreamTransform(bones_trn[i]); }
-        pose = input;
 
         count = bones_trn.Count;
+    }
+
+    public void SetInputPlayable(Playable owner, Playable input, PlayableGraph graph)
+    {
+        owner.SetInputCount(1);
+        pose_playable = (ScriptPlayable<PoseBehaviour>)input;
+        graph.Connect(input, 0, owner, 0);
+        owner.SetInputWeight(0, 1);
     }
 
     public void ProcessRootMotion(AnimationStream stream) { }
 
     public void ProcessAnimation(AnimationStream stream)
     {
+        Debug.Log("In ProcessAnimation");
         //Is it faster to just Array.Copy() the whole thing every frame? It is a relatively large array (~55 elements)
         //Then what is the C# equivalent on iterating on pointers to avoid the index lookup overhead?
+        Debug.Log("In ProcessAnimation - past null check");
+        PoseBehaviour pose = pose_playable.GetBehaviour();
         for (int i = 0; i < count; i++)
         {
+            Debug.Log("Index[" + i + "] setting rotation[" + pose.ExposeBone(i) + "]");
             bones[i].SetLocalRotation(stream, pose.ExposeBone(i));
         }
+    }
+
+    public void Dispose()
+    {
+        bones.Dispose();
     }
 }
 
@@ -147,8 +163,10 @@ public class RawRotationTesting : MonoBehaviour
 {
     private PlayableGraph graph;
     private AnimationPlayableOutput output;
+    private ScriptPlayableOutput output2;
     private AnimationScriptPlayable playable;
-    private RawRotationJob job;
+    private PoseToAnimation job;
+    private TPoseBehaviour tpose_behaviour;
 
     // Start is called before the first frame update
     void OnEnable()
@@ -156,32 +174,34 @@ public class RawRotationTesting : MonoBehaviour
         Animator animator = GetComponent<Animator>();
         graph = PlayableGraph.Create("T.Pose Test_" + UnityEngine.Random.Range(0.0f, 1.0f));
         output = AnimationPlayableOutput.Create(graph, "output", animator);
+        //output2 = ScriptPlayableOutput.Create(graph, "fittizio");
 
-        
+        tpose_behaviour = new TPoseBehaviour();
+        tpose_behaviour.Initialize(animator);
+        var tpose_playable = ScriptPlayable<TPoseBehaviour>.Create(graph, tpose_behaviour);
+
         List<Transform> trn = new List<Transform>(animator.avatar.humanDescription.human.Length);
-        List<Quaternion> q = new List<Quaternion>(animator.avatar.humanDescription.human.Length);
 
         for (int i = 0; i < animator.avatar.humanDescription.human.Length; i++)
         {
-            
             int index = LookUpBone(animator.avatar.humanDescription.human[i].humanName);
             trn.Add(animator.GetBoneTransform((HumanBodyBones)index));
-
-            int skeleton_index = LookUpSkeleton(animator.avatar.humanDescription.human[i].boneName, animator.avatar.humanDescription);
-            q.Add(animator.avatar.humanDescription.skeleton[skeleton_index].rotation);
         }
 
-        job = new RawRotationJob();
-        job.Setup(animator, trn, q);
+        job = new PoseToAnimation();
+        job.Setup(animator, trn);
 
         playable = AnimationScriptPlayable.Create(graph, job);
+        job.SetInputPlayable(playable, tpose_playable, graph);
         output.SetSourcePlayable(playable);
+        //output2.SetSourcePlayable(tpose_playable);
         graph.Play();
     }
 
     private void OnDisable()
     {
         job.Dispose();
+        tpose_behaviour.Dispose();
 
         if (graph.IsValid())
         {
